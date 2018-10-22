6fc2e0d8416dc387ab843bfb369b2daa452845b4
vagrant <vagrant@homestead>
2018-10-22 10:19:16 +0000

    Added Comments to Repository class

--- app/Podcasts/Repositories/PodcastRepository.php
+++ app/Podcasts/Repositories/PodcastRepository.php
@@ -12,37 +12,59 @@ class PodcastRepository {
 
     protected $model;
 
+  /**
+  * PodcastRepository constructor.
+  * @param Podcast $podcast
+  */
     public function _construct(Podcast $podcast)
     {
-      $this->model = $podcast;
+        $this->model = $podcast;
     }
 
+    /**
+    * @param Podcast $podcast
+    * @return Podcast
+    * @throws CreatePodcastErrorException
+    */
     public function createPodcast(array $data) : Podcast {
-      try {
-          return $this->model->create($data);
-      } catch (QueryException $e) {
-          throw new CreatePodcastErrorException($e);
-      }
+        try {
+            return $this->model->create($data);
+        } catch (QueryException $e) {
+            throw new CreatePodcastErrorException($e);
+        }
     }
 
+    /**
+    * @param int $id
+    * @return Podcast
+    * @throws PodcastNotFoundException
+    */
     public function findPodcast(int $id) : Podcast {
-      try{
-        return $this->model->findOrFail($id);
-      } catch (ModelNotFoundException $e){
-        throw new PodcastNotFoundException($e);
-      }
+        try{
+          return $this->model->findOrFail($id);
+        } catch (ModelNotFoundException $e){
+          throw new PodcastNotFoundException($e);
+        }
     }
 
+    /**
+    * @param array $data
+    * @return bool
+    * @throws UpdatePodcastErrorException
+    */
     public function updatePodcast(array $data) : bool {
-      try{
-        return $this->model->update($data);
-      } catch (QueryException $e) {
-        throw new UpdatePodcastErrorException($e);
-      }
+        try{
+          return $this->model->update($data);
+        } catch (QueryException $e) {
+          throw new UpdatePodcastErrorException($e);
+        }
     }
 
+    /**
+    * @return bool
+    */
     public function deletePodcast() : bool{
-      return $this->model->delete();
+        return $this->model->delete();
     }
 
 }
